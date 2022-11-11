d8890592d24dee1152a9d67a26d39339b687b11a
sfragkoul <sfragkoul@certh.gr>
2022-11-11 12:27:28 +0200

    update commands.sh in day 2

--- day 2/commands.sh
+++ day 2/commands.sh
@@ -1,3 +1,7 @@
+##################################
+#     User defined pipeline      #
+################################## 
+
 #create baits.bed
 #uses .bed file and caverage from .bam to infer the targeted regions 
 guess_baits.py Tumor.bam -t gene_targets.bed -o baits.bed
@@ -16,7 +20,51 @@ cnvkit.py coverage Normal.bam baits_target.bed -o Normal.targetcoverage.cnn
 cnvkit.py coverage Normal.bam my_antitargets.bed -o Normal.antitargetcoverage.cnn
 
 #copy number reference from Normal.bam
-cnvkit.py reference Normal.targetcoverage.cnn --fasta reference.fasta -o my_reference.cnn
+cnvkit.py reference *Normal.{,anti}targetcoverage.cnn --fasta reference.fasta -o my_reference.cnn
 
 #combine the uncorrected target and antitarget coverage tables (.cnn) and correct for biases
 cnvkit.py fix Tumor.targetcoverage.cnn Tumor.antitargetcoverage.cnn my_reference.cnn -o Tumor.cnr
+cnvkit.py fix Normal.targetcoverage.cnn Normal.antitargetcoverage.cnn my_reference.cnn -o Normal.cnr
+
+#infer discrete copy number segments from the given coverage table
+cnvkit.py segment Tumor.cnr -o Tumor.cns
+
+#given segmented log2 ratio estimates (.cns), derive each segment’s absolute integer copy number
+cnvkit.py call Tumor.cns -o Tumor.call.cns -v VarScan_somatic.vcf
+
+#plot bin-level log2 coverages and segmentation calls together
+cnvkit.py scatter Tumor.cnr -s Tumor.cns  -v VarScan_somatic.vcf -o Tumor-scatter.pdf
+
+#we can try genemetrics both with and without the segment files, 
+#take the intersection of those as a list of “trusted” genes, and visualize each of them
+cnvkit.py genemetrics -y Tumor.cnr -s Tumor.cns | tail -n+2 | cut -f1 | sort > segment-genes.txt
+cnvkit.py genemetrics -y Tumor.cnr | tail -n+2 | cut -f1 | sort > ratio-genes.txt
+comm -12 ratio-genes.txt segment-genes.txt > trusted-genes.txt
+mkdir gene_scatter_plots
+
+for gene in `cat trusted-genes.txt`
+do
+    cnvkit.py scatter -s Tumor.cn{s,r} -g $gene -o gene_scatter_plots/Tumor-$gene-scatter.pdf
+done
+
+##################################
+#      Automated pipeline        #
+################################## 
+
+#create baits.bed
+#uses .bed file and caverage from .bam to infer the targeted regions 
+guess_baits.py Tumor.bam -t gene_targets.bed -o baits.bed
+
+#with batch command
+cnvkit.py batch Tumor.bam --normal Normal.bam  --targets baits.bed --annotate refFlat.txt  --fasta reference.fasta --access access-5kb-mappable.hg19_chr5_chr12_chr17.bed --output-reference my_reference.cnn --output-dir results/ --diagram --scatter
+
+#we can try genemetrics both with and without the segment files, 
+#take the intersection of those as a list of “trusted” genes, and visualize each of them
+cnvkit.py genemetrics -y Tumor.cnr -s Tumor.cns | tail -n+2 | cut -f1 | sort > segment-genes.txt
+cnvkit.py genemetrics -y Tumor.cnr | tail -n+2 | cut -f1 | sort > ratio-genes.txt
+comm -12 ratio-genes.txt segment-genes.txt > trusted-genes.txt
+
+for gene in `cat trusted-genes.txt`
+do
+    cnvkit.py scatter -s Tumor.cn{s,r} -g $gene -o Tumor-$gene-scatter.pdf
+done
